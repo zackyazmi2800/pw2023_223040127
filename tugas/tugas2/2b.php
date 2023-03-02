05a63e616672de89ee82c3c3d517a5a05faed9f7
zacky azmi asikin <zack@zackys-MacBook-Pro.local>
2023-03-02 19:09:33 +0700

    mengerjakan tugas 2b

--- tugas/tugas2/2b.php
+++ tugas/tugas2/2b.php
@@ -7,45 +7,11 @@
     <title>Tugas 2b</title>
 </head>
 <body>
-    <?php for ($i=1; $i <= 1 ; $i++) : ?>
-        <?= $i ?>
+    <?php for ($i = 1; $i <= 10 ; $i++) : ?>
+        <?php for ($l = 1; $l <= $i ; $l++) :?>
+            <?= $l?>
+            <?php endfor; ?>
+            <br>
     <?php endfor; ?>
-    <br>
-    <?php for ($i=1; $i <= 2 ; $i++) : ?>
-        <?= $i ?>
-    <?php endfor; ?>
-    <br>
-    <?php for ($i=1; $i <= 3 ; $i++) : ?>
-        <?= $i ?>
-    <?php endfor; ?>
-    <br>
-    <?php for ($i=1; $i <= 4 ; $i++) : ?>
-        <?= $i ?>
-    <?php endfor; ?>
-    <br>
-    <?php for ($i=1; $i <= 5 ; $i++) : ?>
-        <?= $i ?>
-    <?php endfor; ?>
-    <br>
-    <?php for ($i=1; $i <= 6 ; $i++) : ?>
-        <?= $i ?>
-    <?php endfor; ?>
-    <br>
-    <?php for ($i=1; $i <= 7 ; $i++) : ?>
-        <?= $i ?>
-    <?php endfor; ?>
-    <br>
-    <?php for ($i=1; $i <= 8 ; $i++) : ?>
-        <?= $i ?>
-    <?php endfor; ?>
-    <br>
-    <?php for ($i=1; $i <= 9 ; $i++) : ?>
-        <?= $i ?>
-    <?php endfor; ?>
-    <br>
-    <?php for ($i=1; $i <= 10 ; $i++) : ?>
-        <?= $i ?>
-    <?php endfor; ?>
-    <br>
 </body>
 </html>
