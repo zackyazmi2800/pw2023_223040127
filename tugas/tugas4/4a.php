db52da526a476381890d51ea6b162f193290b04d
zacky azmi asikin <zack@zackys-MacBook-Pro.local>
2023-03-11 06:05:58 +0700

    mengerjakan tugas 4a

--- tugas/tugas4/4a.php
+++ tugas/tugas4/4a.php
@@ -9,7 +9,7 @@ $kalimat = ['epan', 'cipta', 'akan', 'u', 'asa', 'esok'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>tugas 4a</title>
 </head>
 <body>
     <p>M<?= $kalimat[4]; ?> d<?= $kalimat[0]; ?> di<?= $kalimat[1]; ?>kan oleh apa yang kau kerj<?= $kalimat[2]; ?> hari ini, 
